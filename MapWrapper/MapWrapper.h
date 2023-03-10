8c335aa60e107c275cba5fd77a1fab7eee30fd1a
Miguel de Icaza <miguel@gnome.org>
2023-03-10 10:38:57 -0500

    Initial Commit

---
+++ MapWrapper/MapWrapper.h
@@ -0,0 +1,18 @@
+//
+//  MapWrapper.h
+//  MapWrapper
+//
+//  Created by Miguel de Icaza on 3/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapWrapper.
+FOUNDATION_EXPORT double MapWrapperVersionNumber;
+
+//! Project version string for MapWrapper.
+FOUNDATION_EXPORT const unsigned char MapWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapWrapper/PublicHeader.h>
+
+
